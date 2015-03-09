5987dad0a18fb2f7f750e38aa6aafb072dcc746a
Rafael Almeida <rafael.almeida@outlinksoftware.com>
2015-03-09 17:18:33 +0000

    Rookie's mistake correction

--- Google Chrome/background.js
+++ Google Chrome/background.js
@@ -2,7 +2,7 @@ var config = {
     mode: "pac_script",
     pacScript: {
         data: "function FindProxyForURL(url, host) {\n" +
-                "  if (host == 'thepiratebay.se')\n" +
+                "  if (host === 'thepiratebay.se')\n" +
                 "    return 'PROXY 177.71.192.151:80';\n" +
                 "  return 'DIRECT';\n" +
                 "}"
